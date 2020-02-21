6ea09fb1251e8f1897b55d0b9e629a0deaf4e704
gwenf <gwenfaraday@pm.me>
2020-02-20 20:01:08 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-content>
+      <Playground />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Playground from '@/views/Playground'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    Playground
+  },
+
+  data: () => ({
+    //
+  })
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/Playground.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1>Playground</h1>
+        <hr />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
